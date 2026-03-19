a237d5fdea1c159521bae1eb6b669fa3cd45626e
titan-spyer <mr.satya02@gmail.com>
2026-03-19 16:28:36 +0530

    feat(eda): add missing value analysis for housing dataset

---
+++ price_predictor_system/analysis/analyze_src/missing_value_analysis.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Abstract Base class for missing Value analysis.
+class MissingValueAnalysisTemplate(ABC):
+    def analyze(self, df: pd.DataFrame):
+        self.missing_values_identification(df)
+        self.visualize_missing_values(df)
+
+
+    @abstractmethod
+    def missing_values_identification(self, df: pd.DataFrame):
+        pass
+
+    @abstractmethod
+    def visualize_missing_values(self, df: pd.DataFrame):
+        pass
+
+# concrete  class for missing value analysis.
+class SimpleMissingValueAnalysis(MissingValueAnalysisTemplate):
+    def missing_values_identification(self, df: pd.DataFrame):
+        print("\nMissing Values Count by Column:")
+        missing_values = df.isnull().sum()
+        print(missing_values[missing_values > 0])
