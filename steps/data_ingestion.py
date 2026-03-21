706c414351ba6e177355bcd7e8863fd8f0b99fd5
titan-spyer <mr.satya02@gmail.com>
2026-03-21 18:15:43 +0530

    fix(pipeline): add debuging system

--- steps/data_ingestion.py
+++ steps/data_ingestion.py
@@ -6,10 +6,18 @@ from src.data_ingest import DataIngestFactory
 def data_ingestion_step(file_path: str) -> pd.DataFrame:
     """Ingest data from a ZIP file using the appropriate DataIngestor."""
     # Determine the file extension
-    file_extension = ".zip"
+    # file_extension = ".zip"
     
     # Get the appropriate DataIngestor
-    data_ingestor = DataIngestFactory.get_ingestor(file_extension)
+    try:
+        data_ingestor = DataIngestFactory.get_ingestor(".zip")
+    except ValueError as e:
+        print(f".zip failed as error {e}")
+
+    try:
+        data_ingestor = DataIngestFactory.get_ingestor("zip")
+    except ValueError as e:
+        print(f"zip failed as error {e}")
     
     # Ingest the data and load it into a DataFrame
     df = data_ingestor.ingest_data(file_path)
