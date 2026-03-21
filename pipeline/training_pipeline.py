4e4de596b27f2452243945390458bbc34cb8f73f
titan-spyer <mr.satya02@gmail.com>
2026-03-21 17:53:23 +0530

    fix(pipeline): update comment out the zenml library to avoid issue while training through GC

--- pipeline/training_pipeline.py
+++ pipeline/training_pipeline.py
@@ -1,13 +1,13 @@
 from steps.data_ingestion import data_ingestion_step
 from steps.missing_value_handel import handle_missing_values_step
-from zenml import pipeline, step, Model
+# from zenml import pipeline, step, Model
 
 
-@pipeline(
-    model = Model(
-        name = "prices_predictor"
-    ),
-)
+# @pipeline(
+#     model = Model(
+#         name = "prices_predictor"
+#     ),
+# )
 def ml_pipeline():
     """Define an end-to-end machine learning pipeline."""
 
@@ -25,3 +25,5 @@ def ml_pipeline():
 if __name__ == "__main__":
     # Running the pipeline
     run = ml_pipeline()
+    print("Pipeline executed successfully.")
+    print(run.head())
