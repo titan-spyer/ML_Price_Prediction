3f5a4e00b8556bce06b72708b1a1d25ea8c2c915
titan-spyer <mr.satya02@gmail.com>
2026-03-21 13:23:08 +0530

    feat(pipeline): add pipeline training feature

---
+++ steps/data_ingestion.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from src.data_ingest import DataIngestFactory
+from zenml import step
+
+@step
+def data_ingestion_step(file_path: str) -> pd.DataFrame:
+    """Ingest data from a ZIP file using the appropriate DataIngestor."""
+    # Determine the file extension
+    file_extension = ".zip"
+    
+    # Get the appropriate DataIngestor
+    data_ingestor = DataIngestFactory.get_ingestor(file_extension)
+    
+    # Ingest the data and load it into a DataFrame
+    df = data_ingestor.ingest_data(file_path)
+    return df
---
+++ steps/missing_value_handel.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from src.handeling_misssing_values import MissingValueHandlerContext, DropMissingValues, FillMissingValues
+from zenml import step
+
+@step
+def handle_missing_values_step(df: pd.DataFrame, strategy: str = "mean") -> pd.DataFrame:
+    """Handles missing values using MissingValueHandler and the specified strategy."""
+    if strategy == "drop":
+        handler = MissingValueHandlerContext(DropMissingValues(axis=0))
+    elif strategy in ["mean", "median", "mode", "constant"]:
+        handler = MissingValueHandlerContext(FillMissingValues(method=strategy))
+    else:
+        raise ValueError(f"Unsupported missing value handling strategy: {strategy}")
+
+    cleaned_df = handler.handle_missing_values(df)
+    return cleaned_df
